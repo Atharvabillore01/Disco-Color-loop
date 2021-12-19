7eda90d2d84047e5623b6c653758e9c42764b8c4
Atharvabillore01 <91140562+Atharvabillore01@users.noreply.github.com>
2021-12-19 22:57:47 +0530

    Add files via upload

---
+++ Color screen.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include <conio.h>
+#include<time.h>
+#include<windows.h>
+#include <unistd.h>
+#include <stdio.h>
+using namespace std;
+
+int main(){
+     x:for(i=1;i!=0;i++){
+
+    system("color 90");
+    system("color 10");
+   // sleep(1.1);
+    system("color A0");
+   // sleep(1.1);
+   system("color 20");    
+    //sleep(1.1);
+    system("color B0");    
+    //sleep(1.1);
+    system("color 30");
+    //sleep(1.1);
+    system("color C0");
+    //sleep(1.1);
+    system("color 40");
+    //sleep(1.1);
+    system("color D0");
+    //sleep(1.1);
+    system("color 50");    
+    //sleep(1.1);
+    system("color E0");    
+    //sleep(1.1);
+    system("color 60");
+    //sleep(1.1);
+    system("color F0");
+    //sleep(1.1);
+    system("color 70");
+    //press any key for repating the loop and for exiting loop press key n
+    c=getch();
+    if(c=='n')
+    exit(0);
+    else
+    goto x;
+    }
+}
